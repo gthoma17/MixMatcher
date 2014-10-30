672ccf0b4b4dbda6af4c15d6877eb7f42d80fa96
gthoma17 <gthoma17@emich.edu>
2014-10-29 20:05:13 -0400

    still working on web-ui. no major progress made, but some things changed

--- mixApp.py
+++ mixApp.py
@@ -10,12 +10,8 @@ app = web.application(urls, globals())
 allRecipes = recipeTools.RecipeBook("menu.csv")
 allIngredients = allRecipes.allIngredients() 
 
-ingredientForm = form.Form(form.Dropdown("Ingredients", []))
-
 class mixApp:
 	def GET(self): 
-		ingredient_Form = ingredientForm()
-		ingredient_Form.Ingredients.args = allIngredients
 		return render.ingredientSelectPage(allIngredients)
 
 
